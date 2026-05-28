33ff146b9853fb8681cdae83c84aa1aa904085dd
Nícolas Oliveira <nicolasfilipeolvr@gmail.com>
2026-05-28 17:27:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve todos os arquivos estáticos da pasta atual
+// Serve arquivos estáticos da pasta atual
 app.use(express.static(__dirname));
 
-// Redireciona todas as rotas para o index.html
+// Rota principal: envia o index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Para qualquer outra rota (ex: /produtos), também envia o index.html (para rotas do frontend)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Site rodando na porta ${port}`);
+  console.log(`🚀 Site NEXZY rodando em http://localhost:${port}`);
 });
